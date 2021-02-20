a26ae05ad3b104e26f759e91a4efb590f1a8dec4
Myrzaly  Yerzhan <erzhaniitu15@gmail.com>
2021-02-20 20:46:13 +0600

    fix unauthorized

---
+++ src/main/java/com/bezkoder/springjwt/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.bezkoder.springjwt.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class HomeController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Green Tech StartUp Project";
+    }
+}
