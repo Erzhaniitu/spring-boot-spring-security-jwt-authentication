c92c62200cea945f98a320643ecef8234a018847
Myrzaly  Yerzhan <erzhaniitu15@gmail.com>
2021-02-25 00:07:52 +0600

    add swagger ui

---
+++ src/main/java/com/bezkoder/springjwt/exception/GlobalControllerExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.bezkoder.springjwt.exception;
+
+import org.springframework.core.convert.ConversionFailedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.annotation.security.RunAs;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ExceptionHandler(ConversionFailedException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleConversion(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserPrincipalNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleUsernameNotFound(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
--- src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
+++ src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
@@ -60,7 +60,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
 			.antMatchers("/actuator/**").permitAll()
-			.antMatchers("/").permitAll()
+			.antMatchers("/**").permitAll()
+			//.antMatchers("/swagger-ui").permitAll()
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
